9ff25f4b509d5ba42ab7ecda88d14a0b217bc057
Keris Van Engeland <kerisve@hotmail.com>
2014-04-25 15:23:44 +0200

    added xml files

--- MobielePlatformen/gen/com/thomasmore/mobieleplatformen/R.java
+++ MobielePlatformen/gen/com/thomasmore/mobieleplatformen/R.java
@@ -24,10 +24,39 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080017;
+        public static final int bAdjustCall=0x7f08000a;
+        public static final int bAdjustData=0x7f08000d;
+        public static final int bAdjustSMS=0x7f080016;
+        public static final int etCall=0x7f080001;
+        public static final int etCallInternational=0x7f080003;
+        public static final int etData=0x7f08000b;
+        public static final int etFreeCalAn=0x7f080005;
+        public static final int etFreeCallEn=0x7f080007;
+        public static final int etFreeData=0x7f08000c;
+        public static final int etFreeSmsAn=0x7f080014;
+        public static final int etFreeSmsEn=0x7f080013;
+        public static final int etSMS=0x7f080012;
+        public static final int pager=0x7f08000e;
+        public static final int spFreeCallType=0x7f080009;
+        public static final int spFreeSmsType=0x7f080015;
+        public static final int spProvider=0x7f08000f;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080008;
+        public static final int textView3=0x7f080002;
+        public static final int textView4=0x7f080004;
+        public static final int textView9=0x7f080006;
+        public static final int tvBundel=0x7f080010;
+        public static final int tvPrice=0x7f080011;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int call_frag=0x7f030001;
+        public static final int data_frag=0x7f030002;
+        public static final int editbundle=0x7f030003;
+        public static final int home=0x7f030004;
+        public static final int list_item=0x7f030005;
+        public static final int sms_frag=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f070000;
